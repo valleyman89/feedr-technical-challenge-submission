db38c9efb43089916fe70049179ca239242872b8
Steven Rolph <dev@stevenrolph.com>
2022-08-10 11:35:30 +0100

    define summary item count

--- src/client/components/MenuSummary/MenuSummary.jsx
+++ src/client/components/MenuSummary/MenuSummary.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import "./MenuSummary.css";
+import { useMenuContext } from "../../context/MenuContext";
 
 const MenuSummary = () => {
+  const { menu } = useMenuContext();
   return (
     <div className="menu-summary">
       <div className="container">
         <div className="row">
           <div className="col-6 menu-summary-left">
-            <span>6 items</span>
+            <span>{menu.length} item(s)</span>
           </div>
           <div className="col-6 menu-summary-right">
             6x <span className="dietary">ve</span>
