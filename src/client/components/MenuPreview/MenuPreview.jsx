238806948c9692062b2d069e98a19259d25a99e7
Steven Rolph <dev@stevenrolph.com>
2022-08-10 11:33:28 +0100

    define menu preview component

--- src/client/components/MenuPreview/MenuPreview.jsx
+++ src/client/components/MenuPreview/MenuPreview.jsx
@@ -1,47 +1,38 @@
 import React from "react";
 import "./MenuPreview.css";
+import { useMenuContext } from "../../context/MenuContext";
 
 const MenuPreview = () => {
+  const { menu, setMenu } = useMenuContext();
+
+  const handleItemDelete = (itemToDelete) => {
+    setMenu(menu.filter((item) => item.id !== itemToDelete.id));
+  };
+
   return (
     <div className="col-8">
       <h2>Menu preview</h2>
       <ul className="menu-preview">
-        <li className="item">
-          <h2>Dummy item</h2>
-          <p>
-            <span className="dietary">ve</span>
-            <span className="dietary">v</span>
-            <span className="dietary">n!</span>
-          </p>
-          <button className="remove-item">x</button>
-        </li>
-        <li className="item">
-          <h2>Dummy item</h2>
-          <p>
-            <span className="dietary">ve</span>
-            <span className="dietary">v</span>
-            <span className="dietary">n!</span>
-          </p>
-          <button className="remove-item">x</button>
-        </li>
-        <li className="item">
-          <h2>Dummy item</h2>
-          <p>
-            <span className="dietary">ve</span>
-            <span className="dietary">v</span>
-            <span className="dietary">n!</span>
-          </p>
-          <button className="remove-item">x</button>
-        </li>
-        <li className="item">
-          <h2>Dummy item</h2>
-          <p>
-            <span className="dietary">ve</span>
-            <span className="dietary">v</span>
-            <span className="dietary">n!</span>
-          </p>
-          <button className="remove-item">x</button>
-        </li>
+        {menu.length < 1 ? "No items selected..." : null}
+        {menu &&
+          menu.map((item, index) => (
+            <li key={item.id} className="item">
+              <h2>{item.name}</h2>
+              <p>
+                {item.dietaries.map((diet, index) => (
+                  <span key={index} className="dietary">
+                    {diet}
+                  </span>
+                ))}
+              </p>
+              <button
+                onClick={() => handleItemDelete(item)}
+                className="remove-item"
+              >
+                x
+              </button>
+            </li>
+          ))}
       </ul>
     </div>
   );
